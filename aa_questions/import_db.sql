6a943173d193d91deab05cfb48dffc8fda80d15d
David M Gudeman <davidmgudeman@gmail.com>
2023-03-01 09:48:32 -0800

    start seeding methods

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -34,10 +36,44 @@ CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
   question_id INTEGER NOT NULL,
   parent_reply_id INTEGER,
+  replier_id INTEGER NOT NULL,
+  reply_body TEXT NOT NULL,
 
-  
   
   FOREIGN KEY(question_id) REFERENCES questions(id),
-  FOREIGN KEY(parent_reply_id) REFERENCES replies(id)
+  FOREIGN KEY(parent_reply_id) REFERENCES replies(id),
+  FOREIGN KEY(replier_id) REFERENCES users(id)
 
 );
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_liked_id INTEGER,
+  question_liked_id INTEGER,
+
+  FOREIGN KEY(user_liked_id) REFERENCES users(id),
+  FOREIGN KEY(question_liked_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users( fname, lname)
+VALUES
+  ('David', 'Gudeman'),
+  ('Ningxiao', 'Cao'),
+  ('Peter', 'Parker'),
+  ('Xavier', 'Octopus')
+;
+
+INSERT INTO
+  questions(title, body, user_id)
+VALUES
+  ('Where is Peter?', 'I have not seen Peter, Has anyone seen him?', 0),
+  ('SQL', 'What does SQL stand for?', 1),
+  ('ADT', 'What is ADT?', 1)
+;
+
+
+
+
